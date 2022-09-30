cc6996684c161133cedee568d4fb4a0a764cb6ca
twolln <tim-wollner@online.de>
2022-09-30 02:36:31 +0200

    Aufgabe 1: Methode createGermanTranslator in der Klasse TranslatorFactory erschaffen

--- src/org/hbrs/s1/ws22/uebung1/control/TranslatorFactory.java
+++ src/org/hbrs/s1/ws22/uebung1/control/TranslatorFactory.java
@@ -1,4 +1,11 @@
 package org.hbrs.s1.ws22.uebung1.control;
 
 public class TranslatorFactory {
+
+    //Methode erstellt ein neues Objekt der Klasse GermanTranslator
+    public static Translator createGermanTranslator() {
+        GermanTranslator translator = new GermanTranslator();
+        return translator;
+    }
+
 }
