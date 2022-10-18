8e3f857383fc5fc74984f5b9f5b2cd471d9d84fd
twolln <tim-wollner@online.de>
2022-10-19 00:45:56 +0200

    Aufgabe 2: Bereitstellung der Klasse ContainerException

---
+++ src/org/hbrs/se1/ws22/uebung2/ContainerException.java
@@ -0,0 +1,9 @@
+package org.hbrs.se1.ws22.uebung2;
+
+public class ContainerException extends Exception{
+
+    //Konstruktor
+    public ContainerException(String fehlermeldung){
+        super(fehlermeldung);
+    }
+}
