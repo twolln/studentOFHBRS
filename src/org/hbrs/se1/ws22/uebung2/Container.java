475bea3ff3b5bdf648b326a38a3c6cfb000b6579
twolln <tim-wollner@online.de>
2022-10-19 01:34:10 +0200

    Aufgabe 2: Umsetzung der gewünschten Methoden der Klasse Container

--- src/org/hbrs/se1/ws22/uebung2/Container.java
+++ src/org/hbrs/se1/ws22/uebung2/Container.java
@@ -1,6 +1,7 @@
 package org.hbrs.se1.ws22.uebung2;
 
 import java.util.ArrayList;
+import java.util.Objects;
 
 public class Container {
 
@@ -15,28 +16,61 @@ public class Container {
 
 
     //Methode zum Hinzufügen eines Member Objektes
-    public void addMember(Member member) {
+    public void addMember(Member member) throws ContainerException {
 
+        for (Member iMember : arrayList) {
+            if (iMember.toString().equals(member.toString())) {
+                throw new ContainerException("Das Member-Objekt mit der ID " + member.getID() + " ist bereits vorhanden!");
+            }
+        }
+        arrayList.add(member);
     }
 
 
     //Methode zum Entfernen eines Member Objektes
     public String deleteMember(Integer id) {
-
-        return "";
+        for (int i = 0; i < arrayList.size(); i++) {
+            //System.out.println(arrayList.get(i).toString());
+            if (Objects.equals(arrayList.get(i).getID(), id)){
+                arrayList.remove(i);
+                return "Das Member-Objekt mit der ID " + id + " wurde gelöscht!";
+            }
+        }
+        return "Das Member-Objekt mit der ID " + id + " ist nicht im Container enthalten!";
     }
 
 
     //Methode um alle abgespeicherten Member Objekte zu printen
     public void dump() {
-
+        for (Member iMember : arrayList) {
+            System.out.println(iMember.toString());
+        }
     }
 
 
     //Methode gibt Anzahl der gespeicherten Member Objekte zurück
     public int size() {
 
-        return -1;
+        return arrayList.size();
     }
 
+    /*
+    ///////////////////////////////////////////////MAIN-METHODE//////////////////////////////////////////////
+
+    public static void main(String[] args) throws ContainerException {
+        Member m1 = new ConcreteMember(12112);
+        Member m2 = new ConcreteMember(232323);
+
+        Container memberSpeicher = new Container();
+        memberSpeicher.addMember(m1);
+        memberSpeicher.addMember(m2);
+
+
+        System.out.println(memberSpeicher.deleteMember(12));
+        memberSpeicher.dump();
+
+    }
+
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////
+    */
 }
