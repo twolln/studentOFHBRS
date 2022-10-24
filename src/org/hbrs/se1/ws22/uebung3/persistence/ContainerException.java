4fdf0b25a020f24f648eefa86289e19e383df6b3
twolln <tim-wollner@online.de>
2022-10-25 01:07:21 +0200

    Aufgabe 3-2: Die Dateien von Übung 2 in den Ordner von Übung 3 duplizieren

---
+++ src/org/hbrs/se1/ws22/uebung3/persistence/ContainerException.java
@@ -0,0 +1,9 @@
+package org.hbrs.se1.ws22.uebung3.persistence;
+
+public class ContainerException extends Exception{
+
+    //Konstruktor
+    public ContainerException(String fehlermeldung){
+        super(fehlermeldung);
+    }
+}
