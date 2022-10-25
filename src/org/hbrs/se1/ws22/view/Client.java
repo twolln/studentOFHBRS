61fa1c48e13e936565465f7f02e98d351ab5df9b
twolln <tim-wollner@online.de>
2022-10-25 17:32:27 +0200

    Aufgabe 3-2: Bereitstellung der Klasse Client

---
+++ src/org/hbrs/se1/ws22/view/Client.java
@@ -0,0 +1,26 @@
+package org.hbrs.se1.ws22.view;
+
+import org.hbrs.se1.ws22.uebung3.Container;
+import org.hbrs.se1.ws22.uebung3.Member;
+import org.hbrs.se1.ws22.uebung3.exception.ContainerException;
+
+public class Client {
+
+    //========================================================================================================
+    private  final MemberView memberview = new MemberView();
+    private final Container container = Container.getInstance();
+    //========================================================================================================
+
+    public void dump() {
+        memberview.dump(container.getCurrentList());
+    }
+
+    public void addMember(Member member) throws ContainerException {
+        container.addMember(member);
+    }
+
+    public void deleteMember(Integer id) throws ContainerException {
+        container.deleteMember(id);
+    }
+
+}
